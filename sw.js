07aa4e44000feb56d620a388bc0dd0224e7084cb
danicka150 <danicka150@gmail.com>
2026-02-14 12:32:35 +0500

    Refactor index.html with updated structure and stylesкевнкенкенек
    Removed old caching logic and added install event logging.

--- sw.js
+++ sw.js
@@ -1,21 +1,5 @@
-const cacheName = "fsociety-cache-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("install", event => {
+  console.log("Service Worker installed");
 });
 
 
